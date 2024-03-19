3cb2ea3a2a2dc917ef83950f5e9e8906d3840fa7
Manohar Singh <manoharsingh@Manohars-MacBook-Pro.local>
2024-03-19 16:25:41 +0530

    css fixes

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
               },
             }}
             layoutOptions={{
-              height: 400,
+              height: "90vh",
               width: 500,
             }}
           />
